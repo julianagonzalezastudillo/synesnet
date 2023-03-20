b5deb4115c4c3027679d7488794d724e937ec734
julianagonzalezastudillo <68652754+julianagonzalezastudillo@users.noreply.github.com>
2023-03-20 14:04:36 +0100

    xticks

--- plot_connectivity_matrix.py
+++ plot_connectivity_matrix.py
@@ -2,7 +2,6 @@ import os.path
 from scipy import io
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 from matplotlib.colors import DivergingNorm
 import matplotlib.colors
 import matplotlib.gridspec as gridspec
@@ -18,12 +17,13 @@ with open(path + "BN_Atlas_246_LUT_reoriented.txt", "r") as filestream:
     for line in filestream:
         n_name.append(line.split(",")[0])
 
-# num_sub = len(os.listdir(path + 'symetrical_corr_mat'))
 subjects = {'synes': np.arange(1, 18),
             'ctr': np.arange(18, 35)}
+labels = ['Left', 'Right']
+x = [round(len(n_name)/4), round(len(n_name)/4*3)]
 for sub_type in subjects.keys():
-    fig = plt.figure(figsize=(4, 5), dpi=500)
-    gs1 = gridspec.GridSpec(5, 4)
+    fig = plt.figure(figsize=(4.4, 5), dpi=500)
+    gs1 = gridspec.GridSpec(5, 4, figure=fig)
     gs1.update(wspace=0.05, hspace=0.05)  # set the spacing between axes.
     cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["red", "white", "blue"])
     divnorm = DivergingNorm(vmin=-1, vcenter=0, vmax=1)
@@ -39,7 +39,6 @@ for sub_type in subjects.keys():
         Xfc = fc['CorrMatrix']
         np.fill_diagonal(Xfc, 0)
 
-        print(np.max(abs(Xfc)))
         ax = plt.subplot(gs1[i])
         # ax = sns.heatmap(Xfc, square = True, cmap = cmap, norm=divnorm)
         im = ax.imshow(Xfc, cmap=cmap, norm=divnorm)
@@ -50,22 +49,27 @@ for sub_type in subjects.keys():
         ax.axvline(123 - 0.5, color='black', lw=0.4, clip_on=False, linestyle='--')
         ax.axhline(123 - 0.5, color='black', lw=0.4, clip_on=False, linestyle='--')
 
-        #add the column names as labels
+        # add the column names as labels
         ax.set_xticks([])
         ax.set_yticks([])
+        if i == 13 or i == 14 or i == 15 or i == 16:
+            plt.xticks(x, labels, fontsize=5)
+        if (i/4).is_integer():
+            plt.yticks(x, labels, fontsize=5)
         [ax.spines[axis].set_linewidth(0.6) for axis in ['top', 'bottom', 'left', 'right']]
 
     # borders
     fig.subplots_adjust(bottom=0.01)
     fig.subplots_adjust(top=0.99)
     fig.subplots_adjust(right=0.99)
-    fig.subplots_adjust(left=0.01)
-    
-    #colorbar
-    cbar_ax = fig.add_axes([0.28, 0.09, 0.69, 0.05])
+    fig.subplots_adjust(left=0.07)
+
+    # colorbar
+    cbar_ax = fig.add_axes([0.32, 0.09, 0.66, 0.05])
     cbar = fig.colorbar(im, cax = cbar_ax, orientation='horizontal')
     cbar.ax.tick_params(labelsize=6)
     cbar.outline.set_linewidth(0.6)
     plt.subplots_adjust(wspace=0, hspace=0)
     plt.text(0, 1.2, sub_type, horizontalalignment='center')
+    plt.savefig(os.getcwd() + '/plots/connectivity_matrices_{0}'.format(sub_type), transparent=True)
     plt.show()
