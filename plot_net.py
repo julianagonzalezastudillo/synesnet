3fd585d7cb1f0a917c7d6a853dc514778dd6b53f
julianagonzalezastudillo <68652754+julianagonzalezastudillo@users.noreply.github.com>
2023-03-21 15:25:19 +0100

    normalization by max metric across subject types

--- plot_net.py
+++ plot_net.py
@@ -31,61 +31,87 @@ with open(path + "BN_Atlas_246_LUT_reoriented.txt", "r") as filestream:
         n_name.append(line.split(",")[0])
         # n_name_full.append(line.split(",")[1].strip())
 
-# load net metrics all subjects
-Xnet = []
-for sub in np.arange(num_sub)+1:
-    net_file = net_path + '_'.join(('strength', 'Subject' + str(sub).zfill(3)))
-    strength = io.loadmat(net_file)
-    Xnet.append(strength['strength'][0])
+for net_key in ['strength', 'laterality']:
+    # for net_key in ['strength']:
+    # load net metrics all subjects
+    Xnet = []
+    for sub in np.arange(num_sub)+1:
+        net_file = net_path + '_'.join((net_key, 'Subject' + str(sub).zfill(3)))
+        net_values = io.loadmat(net_file)
+        Xnet.append(net_values[net_key][0])
 
-# Xnet = abs(np.array(Xnet))  # take absolute value of connectivity matrices (undirected networks)
-Xnet = np.array(Xnet)
+    # Xnet = abs(np.array(Xnet))  # take absolute value of connectivity matrices (undirected networks)
+    Xnet = np.array(Xnet)
 
-# first 17 subjects correspond synesthetic subjects
-Xnet_syn = Xnet[:17, :]
-Xnet_ctr = Xnet[17:, :]
-Xnet_mean = Xnet_ctr.mean(axis=0)  # mean across subjects
+    # first 17 subjects correspond synesthetic subjects
+    Xnet_syn = Xnet[:17, :]
+    Xnet_ctr = Xnet[17:, :]
 
-#%% t-test
-n_t_val = []
-n_p_val = []
-for n_idx in np.arange(len(n_name)):
-    t_val, p_val = stats.ttest_ind(Xnet_syn[:, n_idx], Xnet_ctr[:, n_idx])
-    n_t_val = np.append(n_t_val, t_val)
-    n_p_val = np.append(n_p_val, p_val)
+    #%% t-test
+    n_t_val = []
+    n_p_val = []
+    for n_idx in np.arange(len(n_name)):
+        t_val, p_val = stats.ttest_ind(Xnet_syn[:, n_idx], Xnet_ctr[:, n_idx])
+        n_t_val = np.append(n_t_val, t_val)
+        n_p_val = np.append(n_p_val, p_val)
 
-#%% print Hub list
-pd.set_option("display.precision", 2)
-df = pd.DataFrame()
-df['node'] = n_name
-df['strength_synes'] = Xnet_syn.mean(axis=0)
-df['strength_ctr'] = Xnet_ctr.mean(axis=0)
-df['t-val'] = n_t_val
-df['p-val'] = n_p_val
+    #%% print Hub list
+    # Mean across subjects
+    Xnet_syn_mean = Xnet_syn.mean(axis=0)
+    Xnet_ctr_mean = Xnet_ctr.mean(axis=0)
 
-print('-'*100)
-print(df[['node', 'strength_synes']].sort_values(by='strength_synes', key=abs).tail(15).to_string())
-print('Average synes strength: {0}'.format(Xnet_syn.mean(axis=0).mean()))
+    pd.set_option("display.precision", 2)
+    df = pd.DataFrame()
+    df['node'] = n_name
+    df['{0}_synes'.format(net_key)] = Xnet_syn_mean
+    df['{0}_ctr'.format(net_key)] = Xnet_ctr_mean
+    df['t-val'] = n_t_val
+    df['p-val'] = n_p_val
 
-print('-'*100)
-print(df[['node', 'strength_ctr']].sort_values(by='strength_ctr', key=abs).tail(15).to_string())
-print('Average ctr strength: {0}'.format(Xnet_ctr.mean(axis=0).mean()))
+    print('-'*100)
+    print(df[['node', '{0}_synes'.format(net_key)]].sort_values(by='{0}_synes'.format(net_key), key=abs).tail(30).to_string())
+    print('Average synes {0}: {1}'.format(net_key, Xnet_syn.mean(axis=0).mean()))
 
-print('-'*100)
-print(df.loc[df['p-val'] < 0.05, ["node", "p-val"]])
+    print('-'*100)
+    print(df[['node', '{0}_ctr'.format(net_key)]].sort_values(by='{0}_ctr'.format(net_key), key=abs).tail(30).to_string())
+    print('Average ctr {0}: {1}'.format(net_key, Xnet_ctr.mean(axis=0).mean()))
 
+    print('-'*100)
+    print(df.loc[df['p-val'] < 0.05, ["node", "p-val"]].sort_values(by='p-val'))
 
-#%% Plot 3D nodes
-fig = plt.figure(figsize=(12, 10), dpi=400)
-gs = gridspec.GridSpec(1, 20, wspace=0.5)
-ax1 = fig.add_subplot(gs[:, 0:19], projection='3d')
-ax2 = fig.add_subplot(gs[:, 19])
+    #%% Plot 3D nodes
+    # normalize by highest values
+    X_max = np.max([Xnet_syn_mean, Xnet_ctr_mean])
+    # Xnet_syn_mean_norm = Xnet_syn_mean/X_max
+    # Xnet_ctr_mean_norm = Xnet_ctr_mean/X_max
+    # X_max_norm = np.max([Xnet_syn_mean_norm, Xnet_ctr_mean_norm])
 
-# X = Xnet_syn.mean(axis=0)
-X = np.where((df['p-val'] > 0.05), 0, df['t-val'])  # t-val higher than 0.05
-ax_1, ax_2 = nodes_3D(X, xyz, n_name, ax1, ax2)
+    for X, X_name in zip([Xnet_syn_mean, Xnet_ctr_mean],
+                         ['{0}_mean_syn'.format(net_key),
+                          '{0}_mean_ctr'.format(net_key)]):
+        fig = plt.figure(figsize=(12, 10), dpi=400)
+        gs = gridspec.GridSpec(1, 20, wspace=0.5)
+        ax1 = fig.add_subplot(gs[:, 0:19], projection='3d', title=X_name)
+        ax2 = fig.add_subplot(gs[:, 19])
 
-plt.tight_layout(pad=0.5)
-plot_name = 'strength_t-val.png'
-plt.savefig(os.getcwd() + '/plots/' + plot_name, transparent=True)
-plt.show()
+        ax_1, ax_2 = nodes_3D(X, xyz, n_name, ax1, ax2, nodes_val_max=X_max)
+
+        plot_name = '{0}_abs.png'.format(X_name)
+        plt.savefig(os.getcwd() + '/plots/' + plot_name, transparent=True)
+        plt.show()
+
+    #%% for t-values
+    # significant t-values
+    X_t_val = np.where((df['p-val'] > 0.05), 0, df['t-val'])  # t-val higher than 0.05
+
+    X_name = '{0}_t-val'.format(net_key)
+    fig = plt.figure(figsize=(12, 10), dpi=400)
+    gs = gridspec.GridSpec(1, 20, wspace=0.5)
+    ax1 = fig.add_subplot(gs[:, 0:19], projection='3d', title=X_name)
+    ax2 = fig.add_subplot(gs[:, 19])
+
+    ax_1, ax_2 = nodes_3D(X_t_val, xyz, n_name, ax1, ax2, nodes_val_max=None)
+
+    plot_name = '{0}_abs.png'.format(X_name)
+    plt.savefig(os.getcwd() + '/plots/' + plot_name, transparent=True)
+    plt.show()
