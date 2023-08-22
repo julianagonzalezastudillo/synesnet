a9b0c97d5a046757d3534bacb5f0613d58cb6b9c
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2023-08-22 13:21:46 +0200

    compute coreness normalized by corness of 100 randomized networks conserving node strength distribution

--- net_core_random.py
+++ net_core_random.py
@@ -14,6 +14,10 @@ class bcolors:
 
 path = '/Users/juliana.gonzalez/ownCloud/graph_analysis/'
 net_path = '/Users/juliana.gonzalez/ownCloud/graph_analysis/net_metrics/'
+
+# Constants
+RAND_PRESERVE_STRENGTH = True
+
 if not os.path.exists(net_path):
     os.makedirs(net_path)
 
@@ -28,8 +32,15 @@ strength_stat = io.loadmat(strength_stat_file)
 idx_select = strength_stat['names_idx'][0]
 
 # for sub_file in os.listdir(path + 'symetrical_corr_mat'):
-strength_select = True
-for sub in range(1, 35):
+strength_select = False
+if RAND_PRESERVE_STRENGTH:
+    selec = '_conserve_strenght_distribution'
+    rand_folder = 'rand_mat_strength(equal)'
+else:
+    selec = ''
+    rand_folder = 'rand_mat'
+
+for sub in range(6, 22):
     sub_file = 'CorrMatrix_Subject{0}'.format(str(sub).zfill(3))
     print(sub_file)
     t = time.time()
@@ -49,26 +60,29 @@ for sub in range(1, 35):
 
     # Open random matrices
     # the randomized matrices (already) only contain Xfc>0
-    fc_rand_file = path + 'rand_mat/' + 'RandMatrices_Subject{0}{1}'.format(str(sub).zfill(3), selec)
+    fc_rand_file = os.path.join(path, rand_folder, 'RandMatrices_Subject{0}{1}'.format(str(sub).zfill(3), selec))
     fc_rand = io.loadmat(fc_rand_file)
     Xfc_rand = deepcopy(fc_rand['RandMatrices'])
     Xfc_rand[Xfc_rand <= 0] = 0
     C_rand = []  # random networks coreness
-    isCore_rand = []
+    # isCore_rand = []
     for m in np.arange(np.shape(Xfc_rand)[0]):
         print('random network n#{0}'.format(m))
         X = Xfc_rand[m]
         np.fill_diagonal(X, 0)
-        t = time.time()
         C, isCore = coreness(X)  # coreness
         C_rand.append(C)
-        isCore_rand.append(isCore)
+        # isCore_rand.append(isCore)
 
-    isC_rand = np.array([isC for isC in isCore_rand])
+    # isC_rand = np.array([isC for isC in isCore_rand])
     C_rand = np.array(C_rand)
     C_rand_mean = np.mean(C_rand, axis=0)
     C_orig, isCore_orig = coreness(Xfc_thr)
     C_norm = C_orig / C_rand_mean  # coreness ratio
+
+    # Replace NaN and infinite values with zeros
+    C_norm[np.isnan(C_norm) | np.isinf(C_norm)] = 0
+
     C_zscore = (C_orig - C_rand_mean) / np.std(C_rand, axis=0)  # coreness z-score
     print(time.time() - t)
 
@@ -76,9 +90,9 @@ for sub in range(1, 35):
     net_file = net_path + '_'.join(('net_metrics', sub_file.split("_")[-1], 'thr.mat'))
     if os.path.exists(net_file):
         Xnet = sio.loadmat(net_file)
-        Xnet.update({'coreness_norm{0}'.format(selec): C_norm,
-                     'coreness_zcore{0}'.format(selec): C_zscore,
-                     'coreness_rand{0}'.format(selec): C_rand})
+        Xnet.update({'coreness_norm_by_rand{0}'.format(selec): np.nan_to_num(C_norm),
+                     'coreness_zcore_by_rand{0}'.format(selec): np.nan_to_num(C_zscore),
+                     'coreness_rand{0}'.format(selec): np.nan_to_num(C_rand)})
     sio.savemat(net_file, Xnet)
 
 
