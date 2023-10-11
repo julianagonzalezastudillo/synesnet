811c51276240e70e4f70e45d2b7b7a9a364b12bd
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2023-10-11 16:14:59 +0200

    create tools file for general purpose functions

---
+++ tools.py
@@ -0,0 +1,12 @@
+import numpy as np
+from scipy import io
+import os.path
+
+
+def load_net_metrics(net_path, net_key, corr_type, num_sub, idx_select):
+    Xnet = np.array([
+        io.loadmat(os.path.join(net_path, f'net_metrics_Subject{str(sub).zfill(3)}{corr_type}'))[net_key][0]
+        for sub in range(1, num_sub + 1)])
+    X_ = np.zeros(np.shape(Xnet))
+    X_[:, idx_select] = Xnet[:, idx_select]
+    return X_
