e0c6ba1175b3cd20668901a4b1d2d83339876c2b
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2023-10-11 17:15:40 +0200

    change variable name

--- tools.py
+++ tools.py
@@ -3,9 +3,9 @@ from scipy import io
 import os.path
 
 
-def load_net_metrics(net_path, net_key, corr_type, num_sub, idx_select):
+def load_net_metrics(net_path, metric, corr_type, num_sub, idx_select):
     Xnet = np.array([
-        io.loadmat(os.path.join(net_path, f'net_metrics_Subject{str(sub).zfill(3)}{corr_type}'))[net_key][0]
+        io.loadmat(os.path.join(net_path, f'net_metrics_Subject{str(sub).zfill(3)}{corr_type}'))[metric][0]
         for sub in range(1, num_sub + 1)])
     X_ = np.zeros(np.shape(Xnet))
     X_[:, idx_select] = Xnet[:, idx_select]
