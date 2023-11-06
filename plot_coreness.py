926bf7d5f3819d826abcd3a6a8764cefead80339
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2023-11-06 20:17:58 +0100

    adjust for modifications in load_net_metrics and stats saved in .csv

--- plot_coreness.py
+++ plot_coreness.py
@@ -1,16 +1,24 @@
-#%% PLOT
+"""
+=================================
+            SYNESNET
+=================================
+Plot coreness results in .png and also save for complementary 3D oplot in matlab
+"""
+
 import os.path
 import numpy as np
 from scipy import io
-import pandas as pd
 import matplotlib.pyplot as plt
 from os import path
+import pandas as pd
 import sys
-sys.path.append(path.abspath('../netviz'))
 from viz.netlocal import plot_3d_local_metric
 from tools import load_net_metrics, load_xyz, load_node_names
+import seaborn as sns
+from matplotlib.ticker import MaxNLocator
 
 
+sys.path.append(path.abspath('../netviz'))
 path = '/Users/juliana.gonzalez/ownCloud/graph_analysis/'
 net_path = os.path.join(path, 'net_metrics', 'new')
 plot_path = os.path.join(os.getcwd(), 'plots', 'glb', 'new')
@@ -19,11 +27,9 @@ coreness_stat_file = os.path.join(plot_path, 'coreness_norm_by_rand_conserve_str
 
 # CONSTANTS
 SELECTION = True  # True: select base on strength significance
+P_VAL = 0.05
 corr_type = '_thr'
 metric = 'coreness'
-
-# number of subjects
-num_sub = len(os.listdir(os.path.join(path, 'symetrical_corr_mat')))
 
 # nodes nodes positions and names
 xyz = load_xyz()
@@ -33,50 +39,29 @@ n_name, n_name_full = load_node_names()
 lh_ind = [index for index, element in enumerate(np.array(n_name)) if element.endswith('_L')]
 rh_ind = [index for index, element in enumerate(np.array(n_name)) if element.endswith('_R')]
 
-# Open strength t-val file
 if SELECTION:
-    strength_stat = io.loadmat(strength_stat_file)
-    idx_select = strength_stat['names_idx'][0]
-    coreness_stat = io.loadmat(coreness_stat_file)
-    idx_select_coreness = coreness_stat['names_idx'][0]
-    idx_select = list(set(idx_select) & set(idx_select_coreness))
+    # Open strength t-val file
+    stats_file = os.path.join(path, 'results', 'stats_results.csv')
+    df_stats = pd.read_csv(stats_file)
+    idx_select = np.array(df_stats['node_idx'][(df_stats['metric'] == metric) & (df_stats['p-val_corrected'] < P_VAL)])
 else:
     idx_select = slice(None)
 
 # Load net metrics for all subjects
-Xnet = load_net_metrics(net_path, metric, corr_type, num_sub, idx_select)
-
-# Create an empty matrix of subjects and nodes and replace the significant nodes with its values
-X_ = np.zeros([np.shape(Xnet)[0], len(n_name)])  # 34x246
-X_[:, idx_select] = Xnet[:, idx_select]
-Xnet = X_
-
-# Split synesthetic and control subjects
-Xnet[np.isnan(Xnet) | np.isinf(Xnet)] = 0
-Xnet_syn = Xnet[:17, :]
-Xnet_ctr = Xnet[17:, :]
+Xnet_syn, Xnet_ctr = load_net_metrics(net_path, metric, idx_select=idx_select)
 
 # Mean across subjects
 Xnet_syn_mean = Xnet_syn.mean(axis=0)
 Xnet_ctr_mean = Xnet_ctr.mean(axis=0)
 
 # Get max and min across all subjects without node selection
-Xnet_ = load_net_metrics(net_path, metric, corr_type, num_sub, slice(None))
-Xnet_syn_mean_ = np.mean(Xnet_[:17, :], axis=0)
-Xnet_ctr_mean_ = np.mean(Xnet_[17:, :], axis=0)
+Xnet_syn_, Xnet_ctr_ = load_net_metrics(net_path, metric, corr_type, idx_select=slice(None))
+Xnet_syn_mean_ = np.mean(Xnet_syn_, axis=0)
+Xnet_ctr_mean_ = np.mean(Xnet_ctr_, axis=0)
 X_max = np.max([Xnet_syn_mean_, Xnet_ctr_mean_])
 X_min = np.min([Xnet_syn_mean_, Xnet_ctr_mean_])
 norm = plt.Normalize(vmin=X_min, vmax=X_max)
 kwargs = {"norm": norm}
-
-# Create DataFrame for results
-pd.set_option("display.precision", 2)
-df = pd.DataFrame({'node': np.array(n_name)[idx_select],
-                   'node_complete': np.array(n_name_full)[idx_select],
-                   'node_idx': idx_select,
-                   f'{metric}_synes': Xnet_syn.mean(axis=0)[idx_select],
-                   f'{metric}_ctr': Xnet_ctr.mean(axis=0)[idx_select],
-                   })
 
 for X, X_name in zip([Xnet_syn_mean, Xnet_ctr_mean],
                      [f'{metric}{corr_type}_mean_syn',
@@ -105,3 +90,34 @@ for X, X_name in zip([Xnet_syn_mean, Xnet_ctr_mean],
         nodes_file = os.path.join(plot_path, f'{X_name}{side}.mat')
         # sio.savemat(nodes_file, Xvalues)
         print(nodes_file)
+
+#%% Plot coreness distribution
+fig, ax = plt.subplots(figsize=(7, 6), dpi=300)
+
+# BLUE
+sns.histplot(Xnet_ctr_mean_, kde=True, ax=ax, label='KDE', color='#203FB6', element="step", linewidth=3)
+kde_line = ax.lines[-1]  # The KDE line is the last line in the axis
+kde_line.set_linewidth(3)
+
+# RED
+sns.histplot(Xnet_syn_mean_, kde=True, ax=ax, label='KDE', color='#F62336', element="step", linewidth=3)
+kde_line = ax.lines[-1]
+kde_line.set_linewidth(3)
+
+plt.xlim(X_min, X_max)
+# plt.xlabel("coreness")
+plt.ylabel(" ")
+ax.yaxis.set_major_locator(MaxNLocator(5))
+ax.xaxis.set_major_locator(MaxNLocator(5))
+
+# Remove the top and right axes
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+
+# save
+plot_name = os.path.join(plot_path, 'coreness_distribution.png')
+# plt.savefig(plot_name, bbox_inches='tight', pad_inches=0, dpi=300, transparent=True)
+plt.show()
