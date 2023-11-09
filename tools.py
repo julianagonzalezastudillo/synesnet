9f238ef44e100941b26e449f62d329b56fdca41b
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2023-11-09 20:23:49 +0100

    update for generic dir

--- tools.py
+++ tools.py
@@ -5,24 +5,19 @@
 This module is design to load data.
 """
 
-from copy import deepcopy
-import numpy as np
 import os.path
-from scipy import io
+from copy import deepcopy
+
+import numpy as np
 import scipy.io as sio
-
-path = '/Users/juliana.gonzalez/ownCloud/graph_analysis/'
-node_file = os.path.join(path, 'BN_Atlas_246_LUT_reoriented.txt')
-results_file = os.path.join(path, 'resultsROI_Subject001_Condition001.mat')
-
-# number of subjects
-num_sub = len(os.listdir(os.path.join(path, 'symetrical_corr_mat')))
+from scipy import io
+from config import DATA_DIR, NET_DIR, NODE_FILE, INFO_FILE, N_SUB, CORR_TYPE
 
 
-def load_net_metrics(net_path, metric, corr_type='_thr', num_sub=num_sub, idx_select=slice(None)):
+def load_net_metrics(metric, corr_type=CORR_TYPE, net_path=NET_DIR, n_sub=N_SUB, idx_select=slice(None)):
     xnet = np.array([
         io.loadmat(os.path.join(net_path, f'net_metrics_Subject{str(sub).zfill(3)}{corr_type}'))[metric][0]
-        for sub in range(1, num_sub + 1)])
+        for sub in range(1, n_sub + 1)])
 
     # Create an empty matrix of subjects and nodes and replace the significant nodes with its values
     x = np.zeros(np.shape(xnet))
@@ -38,14 +33,13 @@ def load_net_metrics(net_path, metric, corr_type='_thr', num_sub=num_sub, idx_se
     return x_syn, x_ctr
 
 
-def load_fc():
+def load_fc(n_sub=N_SUB):
     # Load all connectivity matrices
-    num_sub = len(os.listdir(os.path.join(path, 'symetrical_corr_mat')))
     fc_all = []
-    for sub in np.arange(num_sub) + 1:
+    for sub in np.arange(n_sub) + 1:
         # load correlation
         sub_file = 'CorrMatrix_Subject{0}.mat'.format(str(sub).zfill(3))
-        fc_file = path + 'symetrical_corr_mat/' + sub_file
+        fc_file = DATA_DIR + 'symetrical_corr_mat/' + sub_file
         fc = io.loadmat(fc_file)
 
         # 3. corr[corr>=0] = [0, 1]  # winning matrices !!!
@@ -66,7 +60,7 @@ def load_fc():
 def load_node_names():
     # nodes names
     n_name, n_name_full = [], []
-    with open(node_file, "r") as filestream:
+    with open(NODE_FILE, "r") as filestream:
         for line in filestream:
             name, name_full = line.split(",")[:2]
             n_name.append(name)
@@ -76,7 +70,7 @@ def load_node_names():
 
 
 def load_xyz():
-    xyz = io.loadmat(results_file)['xyz'][0][:-1]
+    xyz = io.loadmat(INFO_FILE)['xyz'][0][:-1]
     xyz = np.array([x[0] for x in xyz])
     return xyz
 
