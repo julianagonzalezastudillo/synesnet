add033b8d1ba4240dd3b562c4416411fbf80b466
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2023-11-13 11:13:45 +0100

    modif to import config.py

--- plot_coreness_FC.py
+++ plot_coreness_FC.py
@@ -1,17 +1,29 @@
+"""
+=================================
+            SYNESNET
+=================================
+Pearson product-moment correlation coefficients.
+Plot least squares polynomial fit between coreness and FC (measure as correlation)
+"""
+
 import os.path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from tools import load_fc, load_net_metrics, load_node_names
-from config import DATA_DIR, PLOT_PATH, P_VAL
+from config import DATA_DIR, P_VAL
 
 
 metric = "coreness"
 
 # Open stats file
-stats_file = os.path.join(DATA_DIR, 'results', 'stats_results.csv')
+stats_file = os.path.join(DATA_DIR, "results", "stats_results.csv")
 df_stats = pd.read_csv(stats_file)
-idx_select = np.array(df_stats['node_idx'][(df_stats['metric'] == metric) & (df_stats['p-val_corrected'] < P_VAL)])
+idx_select = np.array(
+    df_stats["node_idx"][
+        (df_stats["metric"] == metric) & (df_stats["p-val_corrected"] < P_VAL)
+    ]
+)
 
 # nodes positions and names
 n_name, full_n_name = load_node_names()
@@ -29,19 +41,28 @@ Xfc_ctr_mean = Xfc_ctr.mean(axis=0)
 # mean excluding zero
 Xfc_syn_sum_along_axis0 = np.sum(Xfc_syn, axis=0)
 Xfc_syn_non_zero_count = np.count_nonzero(Xfc_syn, axis=0)
-Xfc_syn_mean_non_zero = np.divide(Xfc_syn_sum_along_axis0, Xfc_syn_non_zero_count, where=Xfc_syn_non_zero_count != 0)
+Xfc_syn_mean_non_zero = np.divide(
+    Xfc_syn_sum_along_axis0, Xfc_syn_non_zero_count, where=Xfc_syn_non_zero_count != 0
+)
 
 Xfc_ctr_sum_along_axis0 = np.sum(Xfc_ctr, axis=0)
 Xfc_ctr_non_zero_count = np.count_nonzero(Xfc_ctr, axis=0)
-Xfc_ctr_mean_non_zero = np.divide(Xfc_ctr_sum_along_axis0, Xfc_ctr_non_zero_count, where=Xfc_ctr_non_zero_count != 0)
+Xfc_ctr_mean_non_zero = np.divide(
+    Xfc_ctr_sum_along_axis0, Xfc_ctr_non_zero_count, where=Xfc_ctr_non_zero_count != 0
+)
 
 ncols = 2
 nrows = 3
-for Xnet_mean, Xfc_mean, plot_name in zip([Xnet_syn_mean, Xnet_ctr_mean], [Xfc_syn_mean, Xfc_ctr_mean], ["SYN", "CTR"]):
+for Xnet_mean, Xfc_mean, plot_name in zip(
+    [Xnet_syn_mean, Xnet_ctr_mean], [Xfc_syn_mean, Xfc_ctr_mean], ["SYN", "CTR"]
+):
     fig, axs = plt.subplots(nrows, ncols, figsize=(4 * ncols, 4 * nrows))
     # Loop through node_idx and populate the subplots
-    for node_idx, row, col in zip(idx_select, np.sort(np.tile(np.arange(nrows), ncols)),
-                                  np.tile(np.arange(ncols), nrows)):
+    for node_idx, row, col in zip(
+        idx_select,
+        np.sort(np.tile(np.arange(nrows), ncols)),
+        np.tile(np.arange(ncols), nrows),
+    ):
         # Scatter plot
         idx_non_zero = np.where(Xfc_mean[node_idx] != 0)
         # axs[row, col].scatter(Xnet_mean, Xfc_[:, node_idx, :], alpha=0.5)
@@ -58,15 +79,19 @@ for Xnet_mean, Xfc_mean, plot_name in zip([Xnet_syn_mean, Xnet_ctr_mean], [Xfc_s
         correlation_line = np.poly1d(fit)
 
         # Plot the correlation line
-        # axs[row, col].plot(Xnet_mean, correlation_line(Xnet_mean), color='red',
-        #                    label=f'Correlation Line (r = {correlation:.2f})')
-        axs[row, col].plot(np.sort(x), correlation_line(np.sort(x)),
-                           label=f'(r = {correlation:.2f})', color='red')
+        # axs[row, col].plot(Xnet_mean, correlation_line(Xnet_mean),
+        #                    label=f'(r = {correlation:.2f})', color='red')
+        axs[row, col].plot(
+            np.sort(x),
+            correlation_line(np.sort(x)),
+            label=f"(r = {correlation:.2f})",
+            color="red",
+        )
 
         # Set labels and title
-        axs[row, col].set_xlabel('coreness')
-        axs[row, col].set_ylabel('correlation')
-        axs[row, col].set_title(f'{full_n_name[node_idx]}, C={Xnet_mean[node_idx]:.2f}')
+        axs[row, col].set_xlabel("coreness")
+        axs[row, col].set_ylabel("correlation")
+        axs[row, col].set_title(f"{full_n_name[node_idx]}, C={Xnet_mean[node_idx]:.2f}")
 
         # Add a legend
         axs[row, col].legend()
