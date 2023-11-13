74daaa5b274e93972104f0e0c54ec77311a06daf
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2023-11-13 15:39:26 +0100

    modif to import config.py

--- tools.py
+++ tools.py
@@ -5,19 +5,24 @@
 This module is design to load data.
 """
 
-import os.path
 from copy import deepcopy
-
 import numpy as np
 import scipy.io as sio
 from scipy import io
 from config import DATA_DIR, NET_DIR, NODE_FILE, INFO_FILE, N_SUB, CORR_TYPE
 
 
-def load_net_metrics(metric, corr_type=CORR_TYPE, net_path=NET_DIR, n_sub=N_SUB, idx_select=slice(None)):
-    xnet = np.array([
-        io.loadmat(os.path.join(net_path, f'net_metrics_Subject{str(sub).zfill(3)}{corr_type}'))[metric][0]
-        for sub in range(1, n_sub + 1)])
+def load_net_metrics(
+    metric, corr_type=CORR_TYPE, net_path=NET_DIR, n_sub=N_SUB, idx_select=slice(None)
+):
+    xnet = np.array(
+        [
+            io.loadmat(net_path / f"net_metrics_Subject{str(sub).zfill(3)}{corr_type}")[
+                metric
+            ][0]
+            for sub in range(1, n_sub + 1)
+        ]
+    )
 
     # Create an empty matrix of subjects and nodes and replace the significant nodes with its values
     x = np.zeros(np.shape(xnet))
@@ -38,12 +43,12 @@ def load_fc(n_sub=N_SUB):
     fc_all = []
     for sub in np.arange(n_sub) + 1:
         # load correlation
-        sub_file = 'CorrMatrix_Subject{0}.mat'.format(str(sub).zfill(3))
-        fc_file = DATA_DIR + 'symetrical_corr_mat/' + sub_file
+        sub_file = "CorrMatrix_Subject{0}.mat".format(str(sub).zfill(3))
+        fc_file = DATA_DIR / "symetrical_corr_mat/" / sub_file
         fc = io.loadmat(fc_file)
 
         # 3. corr[corr>=0] = [0, 1]  # winning matrices !!!
-        fc_thr = deepcopy(fc['CorrMatrix'])
+        fc_thr = deepcopy(fc["CorrMatrix"])
         fc_thr[fc_thr <= 0] = 0
         np.fill_diagonal(fc_thr, 0)
 
@@ -70,23 +75,33 @@ def load_node_names():
 
 
 def load_xyz():
-    xyz = io.loadmat(INFO_FILE)['xyz'][0][:-1]
+    xyz = io.loadmat(INFO_FILE)["xyz"][0][:-1]
     xyz = np.array([x[0] for x in xyz])
     return xyz
 
 
 def save_mat_file(X, xyz, rgb_values, n_name, X_name, plot_path):
     # select index for left and right hemisphere
-    lh_ind = [index for index, element in enumerate(np.array(n_name)) if element.endswith('_L')]
-    rh_ind = [index for index, element in enumerate(np.array(n_name)) if element.endswith('_R')]
+    lh_ind = [
+        index
+        for index, element in enumerate(np.array(load_node_names()[0]))
+        if element.endswith("_L")
+    ]
+    rh_ind = [
+        index
+        for index, element in enumerate(np.array(load_node_names()[0]))
+        if element.endswith("_R")
+    ]
 
-    for ind, side in zip([lh_ind, rh_ind, range(len(X))], ('_lh', '_rh', '')):
+    for ind, side in zip([lh_ind, rh_ind, range(len(X))], ("_lh", "_rh", "")):
         Xvalues = {
-            'Xnet': X[ind],
-            'xyz': xyz[ind],
-            'color': rgb_values[ind],
-            'names': np.array(n_name)[np.nonzero(X[ind])[0] + ind[0]],  # to mention only the significant nodes
-            'names_idx': np.nonzero(X[ind])[0]
+            "Xnet": X[ind],
+            "xyz": xyz[ind],
+            "color": rgb_values[ind],
+            "names": np.array(n_name)[
+                np.nonzero(X[ind])[0] + ind[0]
+            ],  # to mention only the significant nodes
+            "names_idx": np.nonzero(X[ind])[0],
         }
 
         sio.savemat(plot_path / f"{X_name}{side}.mat", Xvalues)
