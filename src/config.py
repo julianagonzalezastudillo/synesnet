8900ec6a99447fe2c40a1bbb6db827b5552b97fb
GONZALEZ ASTUDILLO Juliana <julianagonzalezastudillo@gmail.com>
2023-11-17 11:02:56 +0100

    small modif

--- src/config.py
+++ src/config.py
@@ -3,16 +3,23 @@ from pathlib import Path
 
 # File paths or directories
 DATA_DIR = Path("/Users/juliana.gonzalez/ownCloud/graph_analysis/")
-NET_DIR = DATA_DIR / "net_metrics/new"  # net_path
+
+NET_DIR = DATA_DIR / "net_metrics/new/new"  # net_path
 NET_DIR.mkdir(parents=True, exist_ok=True)
+
+RAND_DIR = DATA_DIR / "rand_mat_strength(equal)"
+RAND_DIR.mkdir(parents=True, exist_ok=True)
+
 PLOT_DIR = Path(os.getcwd(), *["plots", "glb", "new"])
 PLOT_DIR.mkdir(parents=True, exist_ok=True)
+
 
 # Files
 NODE_FILE = os.path.join(DATA_DIR, "BN_Atlas_246_LUT_reoriented.txt")
 INFO_FILE = os.path.join(
     DATA_DIR, "resultsROI_Subject001_Condition001.mat"
 )  # results_file
+
 
 # Application settings
 P_VAL = 0.05
